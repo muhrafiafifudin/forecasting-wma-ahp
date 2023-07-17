66a288074f396eea4ee092cc4889fd29d992dcc0
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-07-17 20:40:37 +0700

    Add database compare criteria

---
+++ app/Models/CriteriaCompare.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CriteriaCompare extends Model
+{
+    use HasFactory;
+
+    protected $table = 'criteria_compares';
+
+    protected $guarded = [];
+}
